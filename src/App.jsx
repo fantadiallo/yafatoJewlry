603dc3ad5b06edccfc4d99a637cba8880e5ea183
fantadiallo <fatmbo0003@outlook.com>
2025-06-18 13:59:34 +0200

    refactor: update layout structure to include updated Header and Footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import { Routes, Route } from "react-router-dom";
 import ComingSoon from "./pages/ComingSoon/CommingSoon";
+import Layout from "./components/layout/layout";
 
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<ComingSoon />} />
+      <Route element={<Layout />}>
+        <Route path="/" element={<ComingSoon />} />
+      </Route>
     </Routes>
   );
 }
