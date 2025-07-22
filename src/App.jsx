65cf79bc1370d1da146c39ec7fd3ba1a69b3bc5b
fantadiallo <fatmbo0003@outlook.com>
2025-07-22 16:25:06 +0200

    Update App routing for custom design flow

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,24 @@
 import { Routes, Route } from "react-router-dom";
-import ComingSoon from "./pages/ComingSoon/CommingSoon";
 import Layout from "./components/layout/layout";
-
+import HomePage from "./pages/HomePage/HomePage";
+import ContactPage from "./pages/Contact/Contact";
+import AboutPage from "./pages/About/About";
+import ComingSoon from "./pages/ComingSoon/CommingSoon";
+import ProductListPage from "./pages/ProductListPage/ProductListPage";
+import ProductPage from "./pages/ProductDetails/ProductPage";
+import { CustomePage } from "./pages/CustomePage/CustomePage";
 
 function App() {
   return (
     <Routes>
       <Route element={<Layout />}>
-        <Route path="/" element={<ComingSoon />} />
+        <Route path="/" element={<HomePage />} />
+        <Route path="/contact" element={<ContactPage />} />
+        <Route path="/about" element={<AboutPage />} />
+        <Route path="/comingsoon" element={<ComingSoon />} />
+        <Route path="/products" element={<ProductListPage />} /> {/* ✅ Product List */}
+        <Route path="/product/:id" element={<ProductPage />} />  {/* ✅ Product Detail */}
+        <Route path="/custom" element={<CustomePage />} /> {/* ✅ Custom Page */}
       </Route>
     </Routes>
   );
