544c8c86cb93469b71e82a2ea527bc25bb6d97fd
fantadiallo <fatmbo0003@outlook.com>
2025-08-10 13:56:35 +0200

    Update layout.jsx

--- src/components/layout/layout.jsx
+++ src/components/layout/layout.jsx
@@ -3,16 +3,18 @@ import Header from '../Header/Header';
 import Footer from '../Footer/Footer';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import { ShopifyCartProvider } from '../../context/ShopifyCartContext';
+
 
 export default function Layout() {
   return (
-    <>
+    <ShopifyCartProvider>
       <Header />
       <main className="container my-5">
         <Outlet />
       </main>
       <Footer />
       <ToastContainer position="top-center" autoClose={5000} />
-    </>
+    </ShopifyCartProvider>
   );
 }
