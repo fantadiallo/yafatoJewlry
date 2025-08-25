e3a89a0a44815eb35e98db5d2d6590505d922b8a
fantadiallo <fatmbo0003@outlook.com>
2025-08-25 13:06:16 +0200

    Update layout component

--- src/components/layout/layout.jsx
+++ src/components/layout/layout.jsx
@@ -1,19 +1,21 @@
-import { Outlet } from 'react-router-dom';
-import Header from '../Header/Header';
-import Footer from '../Footer/Footer';
-import { ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
-import { ShopifyCartProvider } from '../../context/ShopifyCartContext';
-
+import { Outlet, useLocation } from "react-router-dom";
+import Header from "../Header/Header";
+import Footer from "../Footer/Footer";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { ShopifyCartProvider } from "../../context/ShopifyCartContext";
 
 export default function Layout() {
+  const location = useLocation();
+  const hideNavAndFooter = location.pathname === "/newsletter";
+
   return (
     <ShopifyCartProvider>
-      <Header />
-      <main className="container my-5">
+      {!hideNavAndFooter && <Header />}
+      <main>
         <Outlet />
       </main>
-      <Footer />
+      {!hideNavAndFooter && <Footer />}
       <ToastContainer position="top-center" autoClose={5000} />
     </ShopifyCartProvider>
   );
