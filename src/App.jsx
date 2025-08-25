db8d42bac114231e5b73e628a3c5b7eea2d76bad
fantadiallo <fatmbo0003@outlook.com>
2025-08-25 13:06:12 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,8 @@ import Layout from "./components/layout/layout";
 import HomePage from "./pages/HomePage/HomePage";
 import ContactPage from "./pages/Contact/Contact";
 import AboutPage from "./pages/About/About";
-import ProductListPage from "./pages/ProductListPage/ProductListPage";
-import ProductPage from "./pages/ProductDetails/ProductPage";
 import { CustomePage } from "./pages/CustomePage/CustomePage";
+import NewsletterPage from "./pages/NewsLetterPage/NewsletterPage";
 
 function App() {
   return (
@@ -13,10 +12,9 @@ function App() {
       <Route element={<Layout />}>
         <Route path="/" element={<HomePage />} />
         <Route path="/contact" element={<ContactPage />} />
+        <Route path="/newsletter" element={<NewsletterPage />} />
         <Route path="/about" element={<AboutPage />} />
-        <Route path="/products" element={<ProductListPage />} /> {/* ✅ Product List */}
-        <Route path="/products/:id" element={<ProductPage />} />  {/* ✅ Product Detail */}
-        <Route path="/custom" element={<CustomePage />} /> {/* ✅ Custom Page */}
+   <Route path="/custom" element={<CustomePage />} /> 
       </Route>
     </Routes>
   );
