806a6802c566ff6cf5d0e1b9f9506ce0bb352936
fantadiallo <fatmbo0003@outlook.com>
2025-08-29 20:53:45 +0200

    feat: update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,8 @@ import ContactPage from "./pages/Contact/Contact";
 import AboutPage from "./pages/About/About";
 import { CustomePage } from "./pages/CustomePage/CustomePage";
 import NewsletterPage from "./pages/NewsLetterPage/NewsletterPage";
+import ProductDetailsPage from "./pages/ProductDetailsPage/ProductDetailsPage";
+import ProductPage from "./pages/ProductPage/ProductPage";
 
 function App() {
   return (
@@ -14,7 +16,9 @@ function App() {
         <Route path="/contact" element={<ContactPage />} />
         <Route path="/newsletter" element={<NewsletterPage />} />
         <Route path="/about" element={<AboutPage />} />
-   <Route path="/custom" element={<CustomePage />} /> 
+   <Route path="/custom" element={<CustomePage />} />
+   <Route path="/products" element={<ProductPage />} />
+   <Route path="/products/:id" element={<ProductDetailsPage />} />
       </Route>
     </Routes>
   );
