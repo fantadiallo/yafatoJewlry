95ecad86dc823b852f0990a16e467e968a71f059
fantadiallo <fatmbo0003@outlook.com>
2025-08-30 11:26:30 +0200

    Show only NewsletterPage as main route

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,10 @@
 import { Routes, Route } from "react-router-dom";
-import Layout from "./components/layout/layout";
-import HomePage from "./pages/HomePage/HomePage";
-import ContactPage from "./pages/Contact/Contact";
-import AboutPage from "./pages/About/About";
-import { CustomePage } from "./pages/CustomePage/CustomePage";
 import NewsletterPage from "./pages/NewsLetterPage/NewsletterPage";
-import ProductDetailsPage from "./pages/ProductDetailsPage/ProductDetailsPage";
-import ProductPage from "./pages/ProductPage/ProductPage";
 
 function App() {
   return (
     <Routes>
-      <Route element={<Layout />}>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/contact" element={<ContactPage />} />
-        <Route path="/newsletter" element={<NewsletterPage />} />
-        <Route path="/about" element={<AboutPage />} />
-   <Route path="/custom" element={<CustomePage />} />
-   <Route path="/products" element={<ProductPage />} />
-   <Route path="/products/:id" element={<ProductDetailsPage />} />
-      </Route>
+      <Route path="/" element={<NewsletterPage />} />
     </Routes>
   );
 }
