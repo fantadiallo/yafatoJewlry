d8a4b744b5c469ac3b35a55477bf3162c51f8f9a
fantadiallo <fatmbo0003@outlook.com>
2025-09-07 16:33:09 +0200

    hotfix(api): neutralize /api/subscribe with always-200 serverless function

--- api/subscribe.js
+++ api/subscribe.js
@@ -1,4 +1,6 @@
-// Minimal neutralizer: always return 200 JSON
+
 module.exports = (req, res) => {
+  if (req.method === 'OPTIONS') return res.status(200).end(); // preflight, just in case
+  res.setHeader('Content-Type', 'application/json');
   res.status(200).json({}); // pretend success
 };
