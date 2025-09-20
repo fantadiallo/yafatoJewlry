e6d782e1c83ce15127e461dae192e38974bf402c
fantadiallo <fatmbo0003@outlook.com>
2025-09-20 15:05:32 +0200

    fix: SCSS paths (Styles/utils), inject variables globally, replace darken()

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
-import './styles/main.scss';
+import './Styles/main.scss';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
