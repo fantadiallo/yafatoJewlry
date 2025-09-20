1c5f084cae962c99cd9adc7e71111f8aaaf8631f
fantadiallo <fatmbo0003@outlook.com>
2025-09-20 15:53:36 +0200

    fix(deploy): SCSS paths + vite alias + contact template + SPA rewrite

--- vite.config.js
+++ vite.config.js
@@ -17,9 +17,7 @@ export default defineConfig({
   css: {
     preprocessorOptions: {
       scss: {
-        additionalData: `
-          @use "@styles/_variables.scss" as *;
-        `
+        additionalData: '@use "@styles/_variables.scss" as *;'
       }
     }
   }
