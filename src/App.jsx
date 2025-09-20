cea61d9e39053dd141ca350591ffa1773eb42bf0
fatou Jallow <128003934+fantadiallo@users.noreply.github.com>
2025-09-20 17:35:49 +0200

    feat: change

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,10 @@
 import { Routes, Route } from "react-router-dom";
 import NewsletterPage from "./pages/NewsletterPage/NewsletterPage";
-import Privacy from "./pages/policies/Privacy";
-import Exchange from "./pages/policies/Exchange";
-import Shipping from "./pages/policies/Shipping";
-import Terms from "./pages/policies/Terms";
-import Legal from "./pages/policies/Legal";
-import Contact from "./pages/policies/Contact";
 
-function App() {
+export default function App() {
   return (
     <Routes>
       <Route path="/" element={<NewsletterPage />} />
-      <Route path="/privacy" element={<Privacy />} />
-      <Route path="/exchange" element={<Exchange />} />
-      <Route path="/shipping" element={<Shipping />} />
-      <Route path="/terms" element={<Terms />} />
-      <Route path="/legal" element={<Legal />} />
-      <Route path="/contact" element={<Contact />} />
     </Routes>
   );
 }
-
-export default App;
